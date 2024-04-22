434de0ade7aee94e3f8d0d720e3a7c9516207744
stungames <galip@stun-games.com>
2024-04-23 00:25:34 +0100

    - sftm

--- build/generated/mame/mame/drivlist.cpp
+++ build/generated/mame/mame/drivlist.cpp
@@ -4,31 +4,13 @@
 #include "drivenum.h"
 GAME_EXTERN(___empty);
 GAME_EXTERN(mk2);
-GAME_EXTERN(mk2chal);
-GAME_EXTERN(mk2r11);
-GAME_EXTERN(mk2r14);
-GAME_EXTERN(mk2r20);
-GAME_EXTERN(mk2r21);
-GAME_EXTERN(mk2r30);
-GAME_EXTERN(mk2r31e);
-GAME_EXTERN(mk2r32e);
-GAME_EXTERN(mk2r42);
-GAME_EXTERN(mk2r91);
+GAME_EXTERN(sftm);
 
-game_driver const* const driver_list::s_drivers_sorted[12] =
+game_driver const* const driver_list::s_drivers_sorted[3] =
 {
 	&GAME_NAME(___empty),
 	&GAME_NAME(mk2),
-	&GAME_NAME(mk2chal),
-	&GAME_NAME(mk2r11),
-	&GAME_NAME(mk2r14),
-	&GAME_NAME(mk2r20),
-	&GAME_NAME(mk2r21),
-	&GAME_NAME(mk2r30),
-	&GAME_NAME(mk2r31e),
-	&GAME_NAME(mk2r32e),
-	&GAME_NAME(mk2r42),
-	&GAME_NAME(mk2r91),
+	&GAME_NAME(sftm),
 };
 
-std::size_t const driver_list::s_driver_count = 12;
+std::size_t const driver_list::s_driver_count = 3;
